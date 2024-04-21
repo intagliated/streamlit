302da4aafd543d1c777a4966929e3cf03dd8e357
Maria Paul T <73158322+intagliated@users.noreply.github.com>
2024-04-21 18:11:19 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 title = st.title('Maximum of 3 numbers:')
 
-numbers =[st.num_input("Input a Number:") for i in range(3)]
+numbers =[st.number_input("Input a Number:") for i in range(3)]
 st.write(max(numbers))
